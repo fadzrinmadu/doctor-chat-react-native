b8a733f65f8818b54ebcf1333d4461536eea776c
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2022-05-22 23:32:05 +0700

    create hospital screen

--- src/pages/Hospitals/index.tsx
+++ src/pages/Hospitals/index.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ILHospitalBG } from '../../assets';
+import ListHospital from '../../components/molecules/ListHospital';
+import { colors, fonts } from '../../utils';
 
 export default function Hospitals() {
   return (
-    <View>
-      <Text>Hospitals Page</Text>
+    <View style={styles.page}>
+      <ImageBackground source={ILHospitalBG} style={styles.background}>
+        <Text style={styles.title}>Nearby Hospitals</Text>
+        <Text style={styles.desc}>3 Tersedia</Text>
+      </ImageBackground>
+      <View style={styles.content}>
+        <ListHospital />
+        <ListHospital />
+        <ListHospital />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: colors.secondary,
+  },
+  content: {
+    flex: 1,
+    marginTop: -30,
+    paddingTop: 30,
+    borderRadius: 20,
+    backgroundColor: colors.white,
+  },
+  background: {
+    height: 240,
+    paddingTop: 30,
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: colors.white,
+    fontFamily: fonts.primary[600],
+  },
+  desc: {
+    fontSize: 14,
+    marginTop: 6,
+    textAlign: 'center',
+    color: colors.white,
+    fontFamily: fonts.primary[300],
+  },
+});
