fc3f2dd1325607fc584bc99f820a3c8fab90290c
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2022-05-22 23:04:48 +0700

    create message screen

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,5 +1,6 @@
 import Header from './Header';
 import NewsItem from './NewsItem';
+import ListDoctor from './ListDoctor';
 import HomeProfile from './HomeProfile';
 import DoctorRated from './DoctorRated';
 import DoctorCategory from './DoctorCategory';
@@ -8,6 +9,7 @@ import ButtonNavigator from './ButtonNavigator';
 export {
   Header,
   NewsItem,
+  ListDoctor,
   HomeProfile,
   DoctorRated,
   DoctorCategory,
